ffcae9602b695ae4b0e0cffd17e91a585e5ed754
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2021-07-11 16:56:24 -0300

    feat: add breakpoints on Sidebar 🍳

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,11 +1,15 @@
 import React from "react";
-import { Flex, useBreakpointValue } from "@chakra-ui/react";
+import { Flex, IconButton, useBreakpointValue, Icon } from "@chakra-ui/react";
 import { Logo } from "../Header/Logo";
 import { NotificationsNav } from "../Header/NotificationsNav";
 import { Profile } from "../Header/Profile";
 import { SearchBox } from "../Header/SearchBox";
+import { useSidebarDrawer } from "../../contexts/SidebarDrawerContext";
+import { RiMenuLine } from "react-icons/ri";
 
 export function Header() {
+  const { onOpen } = useSidebarDrawer();
+
   const isWildVersion = useBreakpointValue({
     base: false,
     lg: true,
@@ -22,6 +26,16 @@ export function Header() {
       px="6"
       align="center"
     >
+      {!isWildVersion && (
+        <IconButton
+          aria-label="Open navigation"
+          icon={<Icon as={RiMenuLine} />}
+          fontSize="24"
+          variant="unstyled"
+          onClick={onOpen}
+          mr="2"
+        ></IconButton>
+      )}
       <Logo />
       {isWildVersion && <SearchBox />}
 
