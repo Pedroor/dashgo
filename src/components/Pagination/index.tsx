405c67c4ced59ef2df08308b2df04b14c68c6870
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2021-07-17 09:43:01 -0300

    feat: adding logic to calculate siblingsCount on pagination 🐱‍🏍

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -2,7 +2,43 @@ import React from "react";
 import { Button, HStack, Stack, Box } from "@chakra-ui/react";
 import { PaginationItem } from "./PaginationItem";
 
-export function Pagination() {
+interface PaginationProps {
+  totalCounterOfRegisters: number;
+  registersPerPage?: number;
+  currentPage?: number;
+  onPageChange: (page: number) => void;
+}
+const siblingsCount = 1;
+
+function generatePagesArray(from: number, to: number) {
+  return [...new Array(to - from)]
+    .map((_, index) => {
+      return from + index + 1;
+    })
+    .filter(page => page > 0);
+}
+
+export function Pagination({
+  totalCounterOfRegisters,
+  registersPerPage = 10,
+  currentPage = 1,
+  onPageChange,
+}: PaginationProps) {
+  const lastPage = Math.floor(totalCounterOfRegisters / registersPerPage);
+
+  const previousPage =
+    currentPage > 1
+      ? generatePagesArray(currentPage - 1 - siblingsCount, currentPage - 1)
+      : [];
+
+  const nextPages =
+    currentPage < lastPage
+      ? generatePagesArray(
+          currentPage,
+          Math.min(currentPage + siblingsCount, lastPage)
+        )
+      : [];
+
   return (
     <Stack
       direction={["column", "row"]}
@@ -15,10 +51,15 @@ export function Pagination() {
         <strong>0</strong> - <strong>10</strong> of <strong>100</strong>
       </Box>
       <Stack direction="row" spacing="2">
-        <PaginationItem number={1} isCurrent />
-        <PaginationItem number={2} />
-        <PaginationItem number={3} />
-        <PaginationItem number={4} />
+        {previousPage.length > 0 &&
+          previousPage.map(page => {
+            return <PaginationItem key={page} number={page} />;
+          })}
+        <PaginationItem number={currentPage} isCurrent />
+        {nextPages.length > 0 &&
+          nextPages.map(page => {
+            return <PaginationItem key={page} number={page} />;
+          })}
       </Stack>
     </Stack>
   );
