025a4ec52ce4b690e1e5e5b2b3616af1c9f38ed6
Pedro Arthur <54337976+Pedroor@users.noreply.github.com>
2021-07-17 09:27:58 -0300

    refactor: Mirage to add pagination 🐱‍🏍

--- src/services/mirage/index.ts
+++ src/services/mirage/index.ts
@@ -1,4 +1,11 @@
-import { createServer, Factory, Model } from "miragejs";
+import {
+  createServer,
+  Factory,
+  Model,
+  Response,
+  ActiveModelSerializer,
+} from "miragejs";
+
 import faker from "faker";
 
 type User = {
@@ -9,15 +16,19 @@ type User = {
 
 export function makeServer() {
   const server = createServer({
+    serializers: {
+      application: ActiveModelSerializer,
+    },
+
     models: {
       user: Model.extend<Partial<User>>({}),
     },
 
-    /// Factory utilizado para gerar registros aleátorios
     factories: {
       user: Factory.extend({
-        name(i: number) {
-          return `User ${i + 1}`;
+        name(i) {
+          return `${faker.name.firstName()} ${faker.name.lastName()}`;
+          // return `User ${i + 1}`;
         },
         email() {
           return faker.internet.email().toLowerCase();
@@ -29,14 +40,39 @@ export function makeServer() {
     },
 
     seeds(server) {
-      server.createList("user", 20);
+      server.createList("user", 200);
     },
 
     routes() {
       this.namespace = "api";
       this.timing = 750;
-      this.get("/users");
+
+      this.get("/users", function (schema, request) {
+        const { page = 1, per_page = 10 } = request.queryParams;
+
+        const total = schema.all("user").length;
+
+        /*
+         Ex: page = 3, per_page = 15.
+         pageStart = 30 ou 2*15 ou (Number(page) - 1) * Number(per_page);
+         pageEnd = 45 ou 30+15 ou pageStart + Number(per_page);
+         vai do registro 30 ao 45
+        */
+
+        const pageStart = (Number(page) - 1) * Number(per_page);
+        const pageEnd = pageStart + Number(per_page);
+
+        const users = this.serialize(schema.all("user")).users.slice(
+          pageStart,
+          pageEnd
+        );
+
+        return new Response(200, { "x-total-count": String(total) }, { users });
+      });
+
+      this.get("/users/:id");
       this.post("/users");
+
       this.namespace = "";
       this.passthrough();
     },
